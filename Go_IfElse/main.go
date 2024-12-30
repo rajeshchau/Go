72953e44a3a430db21532aa0a9cdaff8a956a462
rajesh chaudhari <93049758+rajeshchau@users.noreply.github.com>
2024-12-30 22:25:16 +0530

    Add Go module and example program demonstrating if-else statements

---
+++ Go_IfElse/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("If Else in GoLang")
+	if num := 3; num < 10 {
+		println("Number is less than 10")
+	} else {
+		fmt.Println("Number is not less than 10.")
+	}
+}
